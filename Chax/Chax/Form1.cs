b86e7a0ecd8a661a19a242a462ff90328e0c7c45
D4roo <daramahmoudi@hotmail.com>
2022-02-14 23:14:42 -0800

    First Commit - Upload To GitHub

---
+++ Chax/Chax/Form1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Chax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FilterInfoCollection filterInfoCollection;
+            VideoCaptureDevice CaptureDevice;
+
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboDevice.Items.Add(filterInfo.Name);
+            try { cboDevice.SelectedIndex = 0; } catch { cboDevice.Text = "No Camera Detected"; }
+        }
+    }
+}
