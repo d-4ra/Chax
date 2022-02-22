6dcac7d35e04e21ff36a506b2234c7be2ab46a5e
D4roo <daramahmoudi@hotmail.com>
2022-02-21 18:14:09 -0800

    Update - Console - Progress

--- Chax/Chax/Form1.cs
+++ Chax/Chax/Form1.cs
@@ -50,11 +50,15 @@ namespace Chax
                 //Environment.Exit(0);
                 this.Close();
             }
+            Console.ForegroundColor = ConsoleColor.Blue;
             Print("Camera Found", 40);
+            Console.ForegroundColor = ConsoleColor.White;
             Print("..", 500);
             Print("Starting Camera Sync", 50);
             Print("...", 500);
+            Console.ForegroundColor = ConsoleColor.Blue;
             Print("Sync Completed", 40);
+            Console.ForegroundColor = ConsoleColor.White;
             Print("Visualising", 40);
             Print(".\n.", 650);
 
@@ -82,9 +86,11 @@ namespace Chax
 
         void CorrectConsoleProgress()
         {
-            if (i == 110) //Manual Wait
+            if (i == 130) //Manual Wait
             {
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Print("Visualising Synced", 40);
+                Console.ForegroundColor = ConsoleColor.White;
                 Print("Executing Hack", 40);
                 Print(".\n.\n.", 650);
                 static void Print(string text, int speed = 40)
@@ -96,9 +102,11 @@ namespace Chax
                     }
                     Console.WriteLine();
                 }
+                Console.ForegroundColor = ConsoleColor.Green;
                 Print("Hack Complete", 40);
+                Console.ForegroundColor = ConsoleColor.White;
                 Print("-Camera Bugged-",40);
-                Print(".\n.", 650);
+                Print(".\n.\n.\n.\n.", 30);
                 Print("Exiting Quietly", 40);
                 this.Close();
             }
